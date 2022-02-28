9bca892c421c423c12f741014bdd77789c6c595d
kunxin-chor <chorkunxin@yahoo.com>
2022-02-28 03:40:37 +0000

    Add q01-01

--- setup.js
+++ setup.js
@@ -5,13 +5,16 @@ console.log("Setup Utility for Vue Questions");
 console.log("-------------------------------");
 let name = prompt("Enter your name: ");
 let email = prompt("Enter your email address: ");
+let batch = prompt("Please enter your batch code (eg. tgc16, tgc15 etc.): ");
 
 let configTemplate = `
 let user = {
     'name': '${name}',
-    'email': '${email}'
+    'email': '${email}',
+    'batch': '${batch}'
 }
 
 export default user;
 `
 fs.writeFileSync('config.js', configTemplate);
+console.log("Student data updated.");
