53c910a18b438a20276097d56882eb26b58ba24d
kunxin-chor <chorkunxin@yahoo.com>
2022-02-28 04:04:20 +0000

    Added q1

--- q01-01/tests/unit/example.spec.js
+++ q01-01/tests/unit/example.spec.js
@@ -1,12 +1,14 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('test-q01-01', () => {
+  it('renders hello world when button is clicked',async () => {
+    const wrapper = shallowMount(App, {
+
     })
-    expect(wrapper.text()).toMatch(msg)
+    await wrapper.find("#btn").trigger('click');
+    const span = wrapper.find('#message');
+    expect(span.text().toLowerCase()).toMatch("hello world!");
+    expect(wrapper.find().text()).toMatch(msg)
   })
 })
