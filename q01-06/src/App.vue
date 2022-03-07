e35f9482c2434e0b56a0733aaa88c75ef535cbe9
kunxin-chor <chorkunxin@yahoo.com>
2022-03-07 07:15:33 +0000

    Added question q01-06

---
+++ q01-06/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div id="text">Hello there</div>
+    <button id="btn">Click me</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
+
