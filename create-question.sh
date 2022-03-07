e6b08aac1072c0d1055cb7c04e48748f1e4c6af3
kunxin-chor <chorkunxin@yahoo.com>
2022-03-07 06:36:06 +0000

    Added q5

--- create-question.sh
+++ create-question.sh
@@ -6,3 +6,6 @@ sleep 2s
 cp templates/vue.config.js "$@"/
 cp templates/App.vue "$@"/src/App.vue
 rm "$@"/src/components/HelloWorld.vue
+echo "Installing unit test"
+cd "$@"
+vue add unit-jest
---
+++ q01-05/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <button v-on:click="setApple">Apple</button>
+    <button v-on:click="setOrange">Orange</button>
+    <div style='background-color:red'>Apple</div>
+    <div style='background-color:orange'>Orange</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data:function(){
+    return {
+      'selected':''
+    }
+  },
+  components: {
+    
+  },
+  methods:{
+    'setApple':function(){
+      this.selected = 1;
+    },
+    'setOrange':function(){
+      this.selected = 2;
+    }
+  }
+}
+</script>
+
+
