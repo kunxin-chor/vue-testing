db499c49bd1afbbf96fb0208e8bb5b836e6ed950
kunxin-chor <chorkunxin@yahoo.com>
2022-03-07 08:50:13 +0000

    Added question q01-08

--- q01-07/src/App.vue
+++ q01-07/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <div id="hello" v-if="show==1">Hello there</div>
-    <div id="goodbye" v-else>Goodbye for now!</div>
-    <button id="btn" v-on:click="toggle">Click me</button>
+    <div id="hello">Hello there</div>
+    <div id="goodbye">Goodbye for now!</div>
+    <button id="btn">Click me</button>
   </div>
 </template>
 
@@ -15,13 +15,11 @@ export default {
   },
   data:function(){
     return {
-      'show':false
+   
     }
   },
   methods:{
-    toggle() {
-      this.show = !this.show;
-    }
+   
   }
 }
 </script>
