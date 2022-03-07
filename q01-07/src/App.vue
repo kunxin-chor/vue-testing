1dcf410b9754dd2cf743a28caa02bf8ddc7e1d49
kunxin-chor <chorkunxin@yahoo.com>
2022-03-07 08:12:42 +0000

    Added solutions for q01-07

---
+++ q01-07/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div id="hello" v-if="show==1">Hello there</div>
+    <div id="goodbye" v-else>Goodbye for now!</div>
+    <button id="btn" v-on:click="toggle">Click me</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+   
+  },
+  data:function(){
+    return {
+      'show':false
+    }
+  },
+  methods:{
+    toggle() {
+      this.show = !this.show;
+    }
+  }
+}
+</script>
+
+
