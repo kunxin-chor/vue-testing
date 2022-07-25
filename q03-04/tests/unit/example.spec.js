acddf87084d41c830b270f5d7622b3f057cbdc4a
kunxin-chor <chorkunxin@yahoo.com>
2022-07-25 06:51:04 +0000

    Added test case

--- q03-04/tests/unit/example.spec.js
+++ q03-04/tests/unit/example.spec.js
@@ -1,12 +1,28 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
+describe('App.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    const wrapper = shallowMount(App)
+    const buttons = wrapper.findAll("button");
+    const div = wrapper.findAll('div').at(1);
+
+    await buttons.at(0).trigger('click');
+    expect(div.element.style.height).toBe(60);
+    
+    await buttons.at(1).trigger('click');
+    expect(div.element.style.width).toBe(60);
+
+    await buttons.at(2).trigger('click');
+    expect(div.element.style.backgroundColor).toBe('red');
+
+    await buttons.at(3).trigger('click');
+    expect(div.element.style.backgroundColor).toBe('blue');
+    
+    await buttons.at(4).trigger('click');
+    expect(div.element.style.backgroundColor).toBe('gray');
+    expect(div.element.style.width).toBe(50);
+    expect(div.element.style.width).toBe(50);        
+
   })
 })
