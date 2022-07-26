76718acde085170e49edbf1771417e2ee509dfe4
kunxin-chor <chorkunxin@yahoo.com>
2022-07-26 05:32:00 +0000

    Added q04-03 trafflight component

---
+++ q04-03/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <traffic-light></traffic-light>
+    <button>Go</button>
+    <button>Wait</button>
+    <button>Stop</button>
+  </div>
+</template>
+
+<script>
+import TrafficLight from '@/TrafficLight.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TrafficLight
+  }
+}
+</script>
+
+
+
+    TrafficLight
