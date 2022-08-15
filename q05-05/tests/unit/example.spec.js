9bf6bea075a8f65edaf32ebe1f98ce59bd3d5380
kunxin-chor <chorkunxin@yahoo.com>
2022-08-15 19:16:58 +0000

    Added test

--- q05-05/tests/unit/example.spec.js
+++ q05-05/tests/unit/example.spec.js
@@ -1,12 +1,18 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('App.vue', () => {
+  it('can change the color', async () => {
+    const wrapper = mount(App, {});
+    const inputs = wrapper.findAll('option');
+    await inputs.at(0).setSelected();
+    await inputs.at(0).trigger('input');
+    expect(wrapper.find('#lights').element.style.backgroundColor).toMatch('red');
+    await inputs.at(1).setSelected();
+    await inputs.at(1).trigger('input');
+    expect(wrapper.find('#lights').element.style.backgroundColor).toMatch('green');
+    await inputs.at(2).setSelected();
+    await inputs.at(2).trigger('input');
+    expect(wrapper.find('#lights').element.style.backgroundColor).toMatch('yellow');
   })
 })
