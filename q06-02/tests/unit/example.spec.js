a17868c7dda7f40e9a774864915095a6329a8f42
kunxin-chor <chorkunxin@yahoo.com>
2022-08-16 10:58:29 +0000

    Added solution for q06-02

--- q06-02/tests/unit/example.spec.js
+++ q06-02/tests/unit/example.spec.js
@@ -1,12 +1,12 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('App.vue', () => {
+  it('perimeter', async () => {
+    const wrapper = mount(App, {});
+    const inputs = wrapper.findAll('input');
+    inputs.at(0).element.value = "honey lemon";
+    await inputs.at(0).trigger('input');
+    expect(wrapper.find('#count').text()).toMatch('11')
   })
 })
