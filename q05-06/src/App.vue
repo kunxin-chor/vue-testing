a6fee2c63c3f143e9bd30d8edd7e1fe9dd3173be
kunxin-chor <chorkunxin@yahoo.com>
2022-08-16 08:08:58 +0000

    Added q05-06

---
+++ q05-06/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <input type="checkbox"/><label>A</label>
+    <input type="checkbox"/><label>B</label>
+    <input type="checkbox"/><label>C</label>
+    <input type="checkbox"/><label>D</label>
+    <input type="checkbox"/><label>E</label>
+    <input type="checkbox"/><label>F</label>
+    <div id="result"></div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data:function(){
+    return {
+    
+    }
+  },
+  components: {
+   
+  }
+}
+</script>
+
+
