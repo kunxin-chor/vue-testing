c6603ac97f8511caefb907277504e84a6ecc87fc
kunxin-chor <chorkunxin@yahoo.com>
2022-08-19 07:14:33 +0000

    Updated questions

--- q05-04/tests/unit/example.spec.js
+++ q05-04/tests/unit/example.spec.js
@@ -1,12 +1,18 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('App.vue', () => {
+  it('can change the color', async () => {
+    const wrapper = mount(App, {});
+    const inputs = wrapper.findAll('input');
+    await inputs.at(0).setChecked();
+    await inputs.at(0).trigger('input');
+    expect(wrapper.find('#lights').element.style.backgroundColor).toMatch('red');
+    await inputs.at(1).setChecked();
+    await inputs.at(1).trigger('input');
+    expect(wrapper.find('#lights').element.style.backgroundColor).toMatch('green');
+    await inputs.at(2).setChecked();
+    await inputs.at(2).trigger('input');
+    expect(wrapper.find('#lights').element.style.backgroundColor).toMatch('yellow');
   })
 })
--- q06-01/README.md
+++ q06-01/README.md
@@ -1,7 +1,7 @@
 # q06-01
 
 ## Question
-In App.vue, there are two textboxes, with the id num1 and num2 assigned to them. There is a `<div>` with the id result. 
+In App.vue, there are two textboxes, with the id `num1` and `num2` assigned to them. There is a `<div>` with the id `result`. 
 
 There is a computed property named sum that just returns 0.
 
