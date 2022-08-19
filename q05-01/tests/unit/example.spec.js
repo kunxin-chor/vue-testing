888b9a5e032229b2fe6a4c978621532b7c68f2dc
kunxin-chor <chorkunxin@yahoo.com>
2022-08-19 06:50:13 +0000

    Added unit test

--- q05-01/tests/unit/example.spec.js
+++ q05-01/tests/unit/example.spec.js
@@ -2,7 +2,13 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 describe('update value in input',  () => {
-  it('display the text in the div', async () => {
-    
+  it('renders props.msg when passed', async () => {
+    const wrapper = shallowMount(App, {});
+    wrapper.find('input').element.value = "AP";
+    await wrapper.find('input').trigger('input');
+ 
+    const span = wrapper.find('#display');
+    expect(span.text()).toMatch("AP");
+    expect(wrapper.vm.input).toMatch('AP')
   })
 })
