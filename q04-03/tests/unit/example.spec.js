41ca2e1c20f6a61bc2ac625fbd26326891c8935d
kunxin-chor <chorkunxin@yahoo.com>
2022-08-19 06:49:39 +0000

    Added more fixes

--- q04-03/tests/unit/example.spec.js
+++ q04-03/tests/unit/example.spec.js
@@ -1,12 +1,20 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import App from '@/App.vue'
+import TrafficLight from '@/TrafficLight.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('App.vue', () => {
+  it('renders traffic light', async () => {
+    const wrapper = mount(App)  
+    const trafficLight = wrapper.findComponent(TrafficLight);
+    const buttons = wrapper.findAll("button");
+
+    await buttons.at(0).trigger('click');
+    expect(trafficLight.element.style.backgroundColor).toBe('green');
+    
+    await buttons.at(1).trigger('click');
+    expect(trafficLight.element.style.backgroundColor).toBe('yellow');
+
+    await buttons.at(2).trigger('click');
+    expect(trafficLight.element.style.backgroundColor).toBe('red');
   })
 })
--- q05-01/README.md
+++ q05-01/README.md
@@ -6,27 +6,3 @@ There is one button, one labeled as `Apple`. There is also one `<div>`, showing 
 Using `v-if`, display the Apple `<div>` when the Apple button is clicked. The Apple `<div>` should not be displayed when the Vue app first starts.
 
 Hint: examine the `v-on` code for the the buttons carefully. 
->>>>>>> 33dac7ed3209ff688943f8ea130a0232ac389909
-
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
